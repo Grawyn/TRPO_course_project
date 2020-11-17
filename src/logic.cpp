8e19b65c55bc77edce0bb10691f617edad85d909
Alex Karavaev <caravaev_alexand@mail.ru>
2020-11-17 18:37:54 +0700

    Added logic function

--- src/logic.cpp
+++ src/logic.cpp
@@ -16,3 +16,30 @@ void words::choise(string *word)
         getline(file,*word);
     }
 }
+
+void menu_head()
+{
+    cout << "Welcome to Hangman." << endl;
+    cout << "Choose a menu item:" << endl;
+    cout << "1. Play" << endl;
+    cout << "2. Exit" << endl; 
+}
+
+void main_function(string word)
+{
+    menu_head();
+    int choose;
+    cin >> choose;
+    switch(choose)
+    {
+        case 1:
+        {
+            cout << "Let's play! Try to guess the word!" << endl;
+            string prompt(word.size(),'-');
+            int lives = 6; // Максимальное количество попыток
+            cout << prompt << endl;
+        }
+        case 2:
+            exit(0);
+    }
+}
--- src/logic.hpp
+++ src/logic.hpp
@@ -7,4 +7,6 @@ using namespace std;
 class words{
 public:
     void choise(string *word);
+    void menu_head();
+    void main_function();
 };
