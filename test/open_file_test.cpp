ee8b5d13f5814384b462dc5220541f7c5a6e9433
Alex Karavaev <caravaev_alexand@mail.ru>
2020-11-19 21:40:47 +0700

    Added test for choise function

---
+++ test/open_file_test.cpp
@@ -0,0 +1,39 @@
+#define CATCH_CONFIG_MAIN
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <catch.hpp>
+#include "logic.hpp"
+
+using namespace std;
+
+TEST_CASE("Instuction test", "instruction")
+{
+    GIVEN("file")
+    {
+        string temp;
+        string word;
+        vector<string> words;
+        ifstream test_file("themes/professions.txt");
+        while(!test_file.eof()){
+            getline(test_file,temp);
+            words.push_back(temp);
+        }
+        WHEN("file function")
+        {
+            logic logic;
+            logic.choise(&word);
+            for(long unsigned int i = 0; i < words.size(); i++){
+                if(word == words[i]){
+                    temp = words[i];
+                    break;
+                }
+            }
+            THEN("Changed word")
+            {
+                REQUIRE(word == temp);
+            }
+        }
+    }
+}
