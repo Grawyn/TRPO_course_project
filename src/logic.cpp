449e19a0d8ed108d676ceb6a3b455d9f8dfd2ec4
Alex Karavaev <caravaev_alexand@mail.ru>
2020-11-19 21:40:23 +0700

    Some changes for downloaded words

--- src/logic.cpp
+++ src/logic.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void logic::choise(string *word)
 {
     srand((unsigned)time(NULL));
-    int n = 6; // Количество слов в файле
+    int n = 18; // Количество слов в файле
     int random = rand() % n + 1;
     ifstream file("themes/professions.txt");
     for(int i = 0; i < random; i++){
